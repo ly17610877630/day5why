27cbd0111a38e07ddc4c2c24d72ad6d974b0d0f1
ly17610877630 <719987647.@qq.com>
2020-01-02 19:27:06 +0800

    第二次

--- src/main/java/demo1/Testmmm.java
+++ src/main/java/demo1/Testmmm.java
@@ -4,7 +4,7 @@ public class Testmmm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				System.out.println("helloword");
+				System.out.println("helloword符合法规和的广泛地换个地方");
 	}
 
 }
